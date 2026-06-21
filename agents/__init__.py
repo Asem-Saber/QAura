8e00dd97d3eb2df22b581e2a1c2362a96fa2f5a3
Asem Saber <114337093+Asem-Saber@users.noreply.github.com>
2026-06-21 22:07:22 +0300

    Delete agents directory

--- agents/__init__.py
+++
@@ -1,6 +0,0 @@
-# agents/__init__.py
-"""QAura agent implementations."""
-
-from agents.unit_test_gen import unit_test_gen_node, UnitTestGenerator
-
-__all__ = ["unit_test_gen_node", "UnitTestGenerator"]
